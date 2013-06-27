c511f3ede41e414d27d5b09bff6565342c121bb0
cspada <cspada@exoia.com>
2013-06-27 23:15:19 +0200

    Upgrade to express 3.3.1 Use less middle-ware

--- app.js
+++ app.js
@@ -1,37 +1,38 @@
-
 /**
  * Module dependencies.
  */
 
 var express = require('express'),
-  routes = require('./routes'),
-  api = require('./routes/api');
+        routes = require('./routes'),
+        api = require('./routes/api');
 
 var app = module.exports = express();
 
 // Configuration
 
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.static(__dirname + '/public'));
-  app.use(app.router);
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(require('less-middleware')({ src: __dirname + '/public' }));
+app.use(express.static(__dirname + '/public'));
+app.use(app.router);
+
+app.configure('development', function () {
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+app.configure('production', function () {
+    app.use(express.errorHandler());
 });
 
-app.configure('production', function(){
-  app.use(express.errorHandler());
-});
 
 // Routes
 
 app.get('/', routes.index);
 app.get('/partial/:name', routes.partial);
+
 
 // JSON API
 
@@ -40,8 +41,9 @@ app.get('/api/name', api.name);
 // redirect all others to the index (HTML5 history)
 app.get('*', routes.index);
 
+
 // Start server
 
-app.listen(3000, function(){
-  console.log("Server started");
+app.listen(app.get('port'), function () {
+    console.log('Server listening on port ' + app.get('port'));
 });
